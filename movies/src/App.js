0aff58e0c6620e6f2e058af7f7719e8c1025b427
april <martinezapril56@yahoo.com>
2019-01-21 00:29:38 -0500

    broke something

--- movies/src/App.js
+++ movies/src/App.js
@@ -7,15 +7,17 @@ import Filters from "./filters/Filters";
 
 
 
-class App extends Component {
-  constructor(props){
-    super(props)
+class App extends Component
+{
+  constructor( props )
+  {
+    super( props );
     this.state = {}
     this.performSearch( "a" );
   }
   performSearch(searchTerm){
-    // console.log('search')
-    // const urlString = `https://api.themoviedb.org/3/search/movie?query=${ searchTerm }&api_key=014f86b604a00da321c812939566d32b`
+
+  // const urlString = `https://api.themoviedb.org/3/search/movie?query=${ searchTerm }&api_key=014f86b604a00da321c812939566d32b`
     const urlString =
       'https://api.themoviedb.org/3/movie/now_playing?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=&language=en-US&page=1' +
       searchTerm
@@ -24,71 +26,70 @@ class App extends Component {
       url: urlString,
       success: ( searchResults ) =>
       {
-        console.log( searchResults )
+        console.log( "Fetched data successfully" );
+        // console.log( searchResults )
         const results = searchResults.results
 
 
-        class App extends Component
-        {
-          constructor( props )
-          {
-            super( props );
-            this.state = { filter: "All", query: "", baseurl: "https://api.themoviedb.org/3/serch/movie?apt_key=1b5adf72a72a13bad99b8fc0c68cbo85" };
-            this.filterChangeHandler = this.filterChangeHandler.bind( this );
-            this.searchChangeHander = this.searchChangeHandler.bind( this );
-          }
-          performSearch( value, baseurl )
-          {
-            console.log( "Perform search using moviedb" );
-            let urlString = baseurl;
-            if ( value == "" || value == null )
-            {
-              return;
+class App extends Component
+{
+  constructor( props )
+  {
+    super( props );
+    this.state = { filter: "All", query: "", baseurl: "https://api.themoviedb.org/3/serch/movie?apt_key=1b5adf72a72a13bad99b8fc0c68cbo85" };
+    this.filterChangeHandler = this.filterChangeHandler.bind( this );
+    this.searchChangeHander = this.searchChangeHandler.bind( this );
+    }
+  performSearch( value, baseurl )
+  {
+      console.log( "Perform search using moviedb" );
+      let urlString = baseurl;
+      if ( value === "" || value === null ){
+          return;
             }
-            urlString += "&query=" + value;
+      urlString += "&query=" + value;
 
-            $ajax( {
-              url: urlString,
-              success: ( searchResults ) =>
-              {
-                console.log( "Fetched data successfully" );
-                console.log( searchResults );
+    $ajax( {
+      url: urlString,
+      success: ( searchResults ) =>{
+            console.log( "Fetched data successfully" );
+            console.log( searchResults );
 
-                const results = searchResults.results;
+            const results = searchResults.results;
 
-                var movieRows = [];
+            let movieRows = [];
 
-                results.forEach( ( movie ) =>
+            results.forEach( ( movie ) =>
                 {
-                  console.log( movie );
-                  movie.poster_src = 'https://image.tmdb.org/t/p/w185' + movie.poster_path;
-                  const movieRow = <MovieRow key={movie.id} movie={movie} />
-                  movieRows.push( movieRow );
+                  
+                movie.poster_src = 'https://image.tmdb.org/t/p/w185' + movie.poster_path;
+                const movieRow = <MovieRow key={movie.id} movie={movie} />
+                movieRows.push( movieRow );
                 } );
 
-                this.setState( { rows: movieRows, query: value, baseurl } );
+                this.setState( { rows: movieRows, query: value, baseurl: baseurl } );
               },
               error: ( xhr, status, err ) =>
               {
                 console.error( `Failed to fetch data ${ err }` );
               }
-            } );
-          }
+          } );
+        }
           searchChangeHandler( event )
-          {
+        {
     
-            console.log( event.target.value );
+          console.log( event.target.value );
 
-            this.performSearch( this.state.query, event );
-          }
+          lolthis.performSearch( this.state.query, event );
+        }
           render()
           {
-            return (
-              <div>
-                <table className="titleBar">
-                  <tbody>
-                    <tr>
-                      <td className="logo">
+          return (
+            <div>
+              <table className="titleBar">
+                <tbody>
+                  <tr>
+                    <td className="logo">
                         <img alt="app icon" src="movies_icon.jpg" />
                       </td>
                       <td className="title">
@@ -108,4 +109,4 @@ class App extends Component {
           }
         }
 
-        export default App;
+export default App;
